e6d30b8066b38402108a69d2c347683941dbe059
Devesh Bhardwaj <Devesh.Bhardwaj@hiwipay.com>
2024-03-17 19:17:05 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const panNumber = 12345
+let name="Devesh"
+var email="devesh@gmail.com"
+college="Indian Institute of Information Technology, Pune"
+
+let school; // It is just declared the variable then It's value is undefined
+
+// panNumber=5433  -> const variables can not be changed
+console.log(panNumber);
+console.table({panNumber,name,email,college,school});
+
+
+/*
+    Don't use var because of the reason of functional and block scope issue 
+*/
+  
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all the JS code as newer version
+
+// alert("Hello") -> we are using Node.js not web browser
+
+// number -> 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null -> standalone value 
+// undefined 
+// symbol -> unique
+// object 
+
+console.log(typeof null); // -> object 
+console.log(typeof undefined); // -> undefined 
+console.log(typeof "devesh"); // string
+console.log(typeof 1); // number
+console.log(typeof true); // boolean
+
+
