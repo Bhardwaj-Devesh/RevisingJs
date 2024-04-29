ceea10bd560e81012702777738f65d57782a1e65
Devesh Bhardwaj <Devesh.Bhardwaj@hiwipay.com>
2024-04-29 06:38:10 +0530

    add Theory on DataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,20 +1,32 @@
 "use strict"; // treat all the JS code as newer version
 
 // alert("Hello") -> we are using Node.js not web browser
+/*
+    There are 7 Types of DataTypes in javascript
+    1. number -> 2 to power 53
+    NOTE: There is no other primitive data type for float or double. All kind of numbers
+    are stored in number
 
-// number -> 2 to power 53
-// bigint
-// string
-// boolean => true/false
-// null -> standalone value 
-// undefined 
-// symbol -> unique
-// object 
+    2. bigint
 
+    3. string
+
+    4. boolean => true/false
+
+    5. null -> standalone value 
+    6. undefined 
+    7. symbol -> unique
+    object 
+*/
 console.log(typeof null); // -> object 
+/*  
+Question is Why it is like that typeof null is object?
+Answer : it is a bug in javascript and When they get to know that this bug is created 
+already many websites had created so they leave it as it is.
+*/
+
 console.log(typeof undefined); // -> undefined 
 console.log(typeof "devesh"); // string
 console.log(typeof 1); // number
 console.log(typeof true); // boolean
-
 
---
+++ 01_basics/functions.js
@@ -0,0 +1,19 @@
+
+let a =10;
+{
+    var a;
+    console.log(a);
+    a =1;
+}
+console.log(a);
+
+/*Question is let present is differenct scope all together
+ and it alocate memory in block. 
+ But if we write 
+ let a = 40;
+ Then let is in script scope not in global scope
+
+ 
+
+
+ */
