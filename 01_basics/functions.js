6775d3a84109d9229d0b2e2b9be23f65d428aee9
Devesh Bhardwaj <Devesh.Bhardwaj@hiwipay.com>
2024-05-07 22:45:16 +0530

    add few concepts of functions

---
+++ 01_basics/functions.js
@@ -0,0 +1,23 @@
+
+// function declaration
+function function_name(){
+
+}
+
+
+//Expression Function
+
+const a = function (){
+
+}
+/*
+    we can also give name to expression function which can only use inside itself
+*/
+
+
+// Call back function -> 
+/*
+    jaab kisi function ko call krte time function pass krte hai 
+    to jis function ko pass kiya jata hai us function ko call back function khte hai 
+    or -> jis function me pass kiya ja raha hai ussee higher order function bolte hai
+*/
